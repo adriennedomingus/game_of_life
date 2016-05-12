b32ec0b5324f24229574fdcd984b15a4aa9bbcb7
Adrienne Domingus <adrienne.domingus@gmail.com>
2016-05-12 08:28:13 -0600

    starting board for game of life

---
+++ src/game_of_life/core.clj
@@ -0,0 +1,23 @@
+(ns game-of-life.core)
+
+(defn make-board [height width]
+  (for [y (range height)]
+    (take width (repeat 0 ))))
+
+(defn print-row [row]
+  (clojure.string/join " " row))
+
+(defn print-board [board]
+  (clojure.string/join "\n" (map print-row board)))
+
+(defn neighbors [[x y]]
+  (for [x-shift [1 0 -1]
+        y-shift [1 0 -1]
+        :when (not (= [0 0] [x-shift y-shift]))]
+    [(+ x x-shift) (+ y y-shift)]))
+
+;; evaluate cell's current state
+;; know x y coordinates of current cell
+;; find all cell's neighbors
+;; count living neighbors
+;; evaluate rules for next state
---
+++ test/game_of_life/core_test.clj
@@ -0,0 +1,23 @@
+(ns game-of-life.core-test
+  (:require [clojure.test :refer :all]
+            [game-of-life.core :refer :all]))
+
+(deftest makes-board
+  (is (= 5 (count (make-board 5 5))))
+  (is (= 4 (count (first(make-board 5 4))))))
+
+(deftest represents-row
+  (is (= "0 0 0 0 0" (print-row [0 0 0 0 0]))))
+
+(deftest represents-board
+  (let [board (make-board 2 2)]
+    (is (= "0 0\n0 0" (print-board board)))))
+
+(deftest find-neighbors
+  (is (= 8 (count (neighbors [0 0])))))
+
+(def test-board
+  [[true true] [true true]])
+
+; (deftest processes-board
+  ; (is (= test-board (circle test-board))))
